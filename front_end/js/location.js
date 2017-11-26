3262decbf9a51abceaf71e5a19a6e62bc48e7c77
qtwtp123 <don29365704@gmail.com>
2017-11-26 16:06:21 +0800

    location js fin

--- front_end/js/location.js
+++ front_end/js/location.js
@@ -12,6 +12,38 @@ function geoSuccess(position) {
     var startPos = position;
     document.getElementById('startLat').textContent = startPos.coords.latitude;
     document.getElementById('startLon').textContent = startPos.coords.longitude;
+
+
+
+    var Lon = parseFloat($("#startLon").text());
+    var Lat = parseFloat($("#startLat").text());
+    //alert(Lon+" "+Lat);
+    //$('#upload-location').val('');
+
+    //$( "select# option:checked" ).val();
+    if(Lon>120.214791 && Lon<120.216891){
+        if(Lat>22.996430 && Lat<22.997637)
+            $("#upload-location").val('1'); // selects "guan fu"
+    } 
+    else if(Lon>120.217152 && Lon<120.218704){
+            if(Lat>22.998407 && Lat<22.999077)
+                $("#upload-location").val('2'); // selects "yuan pin"
+    }
+    else if(Lon>120.215482 && Lon<120.217182){
+            if(Lat>23.000521 && Lat<23.001786)
+                $("#upload-location").val('3'); // selects "social"
+    }
+    else if(Lon>120.220100 && Lon<120.222244){
+            if(Lat>22.995908 && Lat<22.998956)
+                $("#upload-location").val('4'); // selects "csie"
+    }
+    else if(Lon>120.219007 && Lon<120.221164){
+            if(Lat>22.992849 && Lat<22.993531)
+                $("#upload-location").val('5'); // selects "senpi"
+    }
+    else
+        $("#upload-location").val('6'); // selects "guan fu"
+    
 };
 
 function geoError(error) {
@@ -22,3 +54,5 @@ function geoError(error) {
     //   2: position unavailable (error response from location provider)
     //   3: timed out
   };
+
+  
