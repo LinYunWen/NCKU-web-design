7582081c990e731741a43e6e246773e3e68e1ae2
qtwtp123 <don29365704@gmail.com>
2017-11-26 17:34:34 +0800

    change form & fix container bug

--- front_end/js/location.js
+++ front_end/js/location.js
@@ -13,36 +13,32 @@ function geoSuccess(position) {
     document.getElementById('startLat').textContent = startPos.coords.latitude;
     document.getElementById('startLon').textContent = startPos.coords.longitude;
 
-
+   
 
     var Lon = parseFloat($("#startLon").text());
     var Lat = parseFloat($("#startLat").text());
-    //alert(Lon+" "+Lat);
-    //$('#upload-location').val('');
-
-    //$( "select# option:checked" ).val();
-    if(Lon>120.214791 && Lon<120.216891){
-        if(Lat>22.996430 && Lat<22.997637)
+    $("#upload-location").val('6');
+    if(Lon>=120.214791 && Lon<=120.216891){
+        if(Lat>=22.996430 && Lat<=22.997637)
             $("#upload-location").val('1'); // selects "guan fu"
     } 
-    else if(Lon>120.217152 && Lon<120.218704){
-            if(Lat>22.998407 && Lat<22.999077)
+    else if(Lon>=120.217152 && Lon<=120.218704){
+            if(Lat>=22.998407 && Lat<=22.999077)
                 $("#upload-location").val('2'); // selects "yuan pin"
     }
-    else if(Lon>120.215482 && Lon<120.217182){
-            if(Lat>23.000521 && Lat<23.001786)
+    else if(Lon>=120.215482 && Lon<=120.217182){
+            if(Lat>=23.000521 && Lat<=23.001786)
                 $("#upload-location").val('3'); // selects "social"
     }
-    else if(Lon>120.220100 && Lon<120.222244){
-            if(Lat>22.995908 && Lat<22.998956)
+    else if(Lon>=120.220100 && Lon<=120.222244){
+            if(Lat>=22.995908 && Lat<=22.998956)
                 $("#upload-location").val('4'); // selects "csie"
     }
-    else if(Lon>120.219007 && Lon<120.221164){
-            if(Lat>22.992849 && Lat<22.993531)
+    else if(Lon>=120.219007 && Lon<=120.221164){
+            if(Lat>=22.992849 && Lat<=22.993531)
                 $("#upload-location").val('5'); // selects "senpi"
     }
-    else
-        $("#upload-location").val('6'); // selects "guan fu"
+   
     
 };
 
