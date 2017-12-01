01cd33292bada0db7f20416c4fc7a8fa04e1a430
Lin Yun Wen <F74031051@mail.ncku.edu.tw>
2017-12-01 22:33:08 +0800

    Feature: add notification

---
+++ front_end/js/publish.js
@@ -0,0 +1,28 @@
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
+document.getElementById("test").addEventListener("click", displayNotification);
+
+function displayNotification() {
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration().then(function(reg) {
+        var options = {
+            body: 'Here is a notification body!',
+            icon: 'img/icons/logo_144.png',
+            vibrate: [100, 50, 100],
+            data: {
+              dateOfArrival: Date.now(),
+              primaryKey: 1
+            },
+            actions: [
+                {action: 'explore', title: 'Explore this new world',
+                  icon: 'img/icons/scan.png'},
+                {action: 'close', title: 'Close notification',
+                  icon: 'img/icons/search2.png'},
+            ]
+        };
+        reg.showNotification('Hello world!', options);
+      });
+    }
+}
